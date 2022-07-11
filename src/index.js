c7f2769b0dcef257754ba343b210b8da6bd8091d
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-07-11 14:41:03 +0300

    Class Component version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
+// component file
+import TodoContainer from "./Component/TodoContainer";
+// stylesheet 
+import './App.css'
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+		<TodoContainer />
+		</React.StrictMode>
+, document.getElementById("root"))
